7b4b98b07c40c249b4c65121f7b008be1c361f6d
Huda Hajira <hudahajira744@gmail.com>
2024-10-09 00:58:12 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+import "./js/slider.js";
+import "./js/close-window.js";
+import "./js/buy-now-modal.js";
+import "./js/reviews-modal.js";
+import "./js/subscribe-modal.js";
+import "./js/burger-menu.js";
+import "./js/scroll-up.js"
