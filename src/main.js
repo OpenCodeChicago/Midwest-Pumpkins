c7721cd2bd29bf6c6a3737ddba9b72f68b79fd1d
minji105 <ajhjmj9@naver.com>
2024-10-13 03:34:23 +0900

    fix hover flicker, enhance feature css

--- src/main.js
+++ src/main.js
@@ -4,5 +4,4 @@ import "./js/buy-now-modal.js";
 import "./js/reviews-modal.js";
 import "./js/subscribe-modal.js";
 import "./js/burger-menu.js";
-import "./js/features.js";
 import "./js/scroll-up.js"
