326da1f86081b78b82c3d79de2b96daced85071b
SreyaSudheeran <sreyasudheeran@gmail.com>
2024-10-15 21:11:05 +0530

    Added user review and review modal

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import "./js/slider.js";
 import "./js/close-window.js";
 import "./js/buy-now-modal.js";
 import "./js/reviews-modal.js";
+import "./js/review.js";
 import "./js/subscribe-modal.js";
 import "./js/burger-menu.js";
 import "./js/features.js";
-import "./js/scroll-up.js"
-import "./js/header.js"
+import "./js/scroll-up.js";
+import "./js/header.js";
