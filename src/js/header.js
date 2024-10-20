b85e371d7cdd1677f4a4a391a36a534c79edee53
Amy Woodworth <woodworthcommunications@gmail.com>
2024-10-19 22:30:02 -0500

    Added js for burger menu. Updated html/css for slideout menu.

--- src/js/header.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('burger-menu').addEventListener('click', function() {
-    document.getElementById('mobile-menu').style.display = 'block';
-  });
-  
-  document.getElementById('close-menu').addEventListener('click', function() {
-    document.getElementById('mobile-menu').style.display = 'none';
-  });
-  
