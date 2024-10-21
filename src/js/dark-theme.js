7361f75f8ee6819dc69ddfef8cfcb938d9e11baa
Alexandrbig1 <alexsmagin1@gmail.com>
2024-10-21 09:32:56 -0500

    fix: update styles

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,13 +1,12 @@
-const themeToggleBtn = document.querySelector('.dark-theme-icon');
+const themeToggleBtn = document.querySelector(".dark-theme-icon");
 
-themeToggleBtn.onclick = function (){
-    alert('Proper dark mode color needs to be added')
-    document.body.classList.toggle('dark-mode');
-    const isDarkMode = document.body.classList.contains('dark-mode')
-    localStorage.setItem('darkMode', isDarkMode)
+themeToggleBtn.onclick = function () {
+  document.body.classList.toggle("dark-mode");
+  const isDarkMode = document.body.classList.contains("dark-mode");
+  localStorage.setItem("darkMode", isDarkMode);
 };
 
-const darkMode = localStorage.getItem('darkMode');
-if(darkMode === 'true'){
-    document.body.classList.toggle('dark-mode');
+const darkMode = localStorage.getItem("darkMode");
+if (darkMode === "true") {
+  document.body.classList.toggle("dark-mode");
 }
